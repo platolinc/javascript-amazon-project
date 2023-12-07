483fd81824c1e342ed2c6b8c47d1377c98e7f71c
platolinc <2686550623@qq.com>
2023-12-07 13:32:34 +0800

    Genarate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,12 @@
-export const cart = [
-    
-]
+export const cart = [{
+  productId:
+  'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+   quantity: 2, 
+},{
+    productId: 
+    '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity: 1
+}];
 
 
 export function addToCart(productId){
@@ -9,7 +15,7 @@ export function addToCart(productId){
         cart.forEach((cartItem)=>{
           if(productId === cartItem.productId)
           matchingItem = cartItem;
-        })
+        });
   
         if(matchingItem){
           matchingItem.quantity += 1;
