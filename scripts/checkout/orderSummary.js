56f259333b0d6873639e8f6a01fa640338c94268
platolinc <2686550623@qq.com>
2023-12-08 21:15:13 +0800

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,7 +3,7 @@ import {products, getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js';
-
+import { renderPaymentSummary } from './paymentSummary.js';
 
 export function renderOrderSummary(){
   let cartSummaryHTML ='';
@@ -123,6 +123,8 @@ export function renderOrderSummary(){
           `.js-cart-item-container-${productId}`
         );
         container.remove();
+
+        renderPaymentSummary();
       });
     });
 
@@ -132,6 +134,7 @@ export function renderOrderSummary(){
           const {productId,deliveryOptionId} = element.dataset;
           updateDeliveryOption(productId,deliveryOptionId);
           renderOrderSummary();
+          renderPaymentSummary();
         });
       });
 }
